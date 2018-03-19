23e379552bf76b7005ccfc072df3fe279b05aad5
Paul Shibanov <paul.shibanov@gmail.com>
2018-03-19 08:57:01 +0100

    Squashed commit of the following:
    commit 36ab20be3cb1ef8fada8e38ffe0a5827761b32ea
    Author: Paul Shibanov <paul.shibanov@gmail.com>
    Date:   Mon Mar 19 08:56:08 2018 +0100
    
        chore: bump version
    
    commit da50f6f492fb67c1d9d21d7c18f1e66a8eaa77e1
    Author: Paul Shibanov <paul.shibanov@gmail.com>
    Date:   Mon Mar 19 08:50:16 2018 +0100
    
        Squashed commit of the following:
    
        commit 0ef6b9ea1fd8bd8b8fdf31e01ab803dcb925a545
        Author: Paul Shibanov <paul.shibanov@gmail.com>
        Date:   Mon Mar 19 08:31:50 2018 +0100
    
            chore: lower ci requirements
    
        commit 418a2d303c89c89716a73b4c89f090f47c4d35eb
        Author: Paul Shibanov <paul.shibanov@gmail.com>
        Date:   Mon Mar 19 08:20:33 2018 +0100
    
            chore: change karma loglevel
    
        commit 20c7f3ad96b9e1fa000664de283484d69cd02039
        Author: Paul Shibanov <paul.shibanov@gmail.com>
        Date:   Mon Mar 19 08:09:27 2018 +0100
    
            feat: add angular material
    
    commit b718646ecf5effffee023d9d1f9e134ba082d923
    Merge: e1d90e2 c7dd24a
    Author: Paul Shibanov <paul.shibanov@gmail.com>
    Date:   Sun Mar 18 00:15:36 2018 +0100
    
        Merge tag 'v0.0.4' into develop
    
        v0.0.4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [ MaterialModule ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
