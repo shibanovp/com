4d727552b829c3d77bc6d6353925e42820738c8e
Paul Shibanov <paul.shibanov@gmail.com>
2018-03-24 11:46:26 +0100

    Squashed commit of the following:
    commit a79ec1b88c17dab524b345c04122a97f0a2a6da6
    Author: Paul Shibanov <paul.shibanov@gmail.com>
    Date:   Sat Mar 24 11:32:46 2018 +0100
    
        chore: display construction message

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('blog App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Yet another blog about software engineering.');
   });
 });
